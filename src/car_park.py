d6b319b846354e48a09ee145202f39629df947c8
w3ngm0 <waggmo@gmail.com>
2024-11-22 11:52:39 +0800

    feat(constructors): Adding constructor to the CarPark class

--- src/car_park.py
+++ src/car_park.py
@@ -4,4 +4,20 @@ Car Park Class below
 
 
 class CarPark:
-    pass
+    # Constructors
+    location = "Unknown"
+    capacity = "Unknown"
+
+    def __init__(self, location, capacity, plates=None, sensors=None, displays=None):
+        self.location = location
+        self.capacity = capacity
+        self.plates = plates or []
+        self.sensors = sensors or []
+        self.displays = displays or []
+
+    """
+    Method caled when printing a car park 'object'. Method should also return string 
+    containing car park's location and capacity. For eg. "Car park at ___ street, with __ bays."
+    """
+    def __str__(self):
+        ... # Return a string containing the car park's location and capacity
