838538eda3516113b29440c1fe42d95ff1389135
w3ngm0 <waggmo@gmail.com>
2024-11-23 06:36:57 +0800

    feat: Adding methods to Sensor class to detect vehicle.

--- src/sensor.py
+++ src/sensor.py
@@ -1,6 +1,9 @@
-class Sensor:
+from abc import ABC, abstractmethod
+import random
+
+class Sensor(ABC):
     """
-    Constructor method for Sensor class.
+    Constructor method for Sensor class. Inherits from ABC abstract method
     """
     def __init__(self, id, is_active, car_park):
         self.id = id
@@ -12,11 +15,32 @@ class Sensor:
 
     """need subclass Entry and Exit for sensor"""
 
+    @abstractmethod
+    def update_car_park(self, plate):
+        """Abstract method"""
+        pass
+
+    def _scan_plate(self):
+        return 'FAKE-' + format(random.randint(0, 999), "03d")
+
+    def detect_vehicle(self):
+        plate = self._scan_plate()
+        self.update_car_park(plate)
+
 
 class EntrySensor(Sensor):
-    ...
+    def update_car_park(self, plate):
+        self.car_park.add_car(plate)
+        print(f"Incoming 🚗 vehicle detected. Plate: {plate}")
 
 
 class ExitSensor(Sensor):
-    ...
+    def update_car_park(self, plate):
+        self.car_park.remove_car(plate)
+        print(f"Incoming 🚗 vehicle detected. Plate: {plate}")
+
+    def _scan_plate(self):
+        return random.choice(self.car_park.plates)
+
+
 
