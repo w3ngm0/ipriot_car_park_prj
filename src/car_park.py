1ffd49688bc40b4f0f5586c598d2a7f2f5fcb674
w3ngm0 <waggmo@gmail.com>
2024-11-22 13:15:57 +0800

    feat: Implementing methods for CarPark class

--- src/car_park.py
+++ src/car_park.py
@@ -1,7 +1,8 @@
 """
-Car Park Class below
+Car Park Class below with imports from Sensor and Display class
 """
-
+from sensor import Sensor
+from display import Display
 
 class CarPark:
     # Constructors
@@ -13,8 +14,24 @@ class CarPark:
         self.displays = displays or []
 
     """
-    Method caled when printing a car park 'object'. Method should also return string 
+    Method called when printing a car park 'object'. Method should also return string 
     containing car park's location and capacity. For eg. "Car park at ___ street, with __ bays."
     """
     def __str__(self):
         ...  # Return a string containing the car park's location and capacity
+
+    # def register(self, component):
+    #     if not isinstance(component, (Sensor, Display)):
+    #         raise TypeError("Object must be a Sensor or Display")
+
+    def register(self, component):
+        """ This method will allow car park to register sensors and displays
+
+        """
+        if isinstance(component, Sensor):
+            self.sensors.append(component)
+        elif isinstance(component, Display):
+            self.displays.append(component)
+        else:
+            raise TypeError("Object must be a Sensor or Display")
+
